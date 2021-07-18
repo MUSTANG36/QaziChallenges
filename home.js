6bf182ad22d28a6c82fbdd921514cdd88d711992
Ricky <ochor01@pfw.edu>
2021-07-18 18:04:06 -0400

    Covert Age to Days
    Complete an easy JavaScript challange

---
+++ home.js
@@ -0,0 +1,51 @@
+// this is a comment line
+
+// console.log('hello');
+
+// alert(10000);
+
+// alert('hello this is qazi');
+
+
+
+// var age = prompt('What is your age?');
+
+// document.getElementById('someText').innerHTML = age;
+
+
+// function greeting(yourName){ 
+//     var result = 'This is your name: '+ yourName;
+//     console.log(result);
+// }
+
+
+// var firstName = prompt('What is your name');
+//  greeting(firstName);
+
+
+function ageInDays(){
+    let brithYear = prompt("What year were you born on?");
+    let cd = new Date();
+    let currentYear = cd.getFullYear();
+
+    let ageInDays = (currentYear - brithYear) * 365;
+
+    //create and store 
+    let h1 = document.createElement('h1');
+    let textAnswer = document.createTextNode('Youa are ' +ageInDays+ 'days');
+    h1.setAttribute('id','ageInDays');
+    h1.appendChild(textAnswer);
+
+    //display
+    document.getElementById('flex-box-result').appendChild(h1);
+    console.log(textAnswer);
+
+    document.getElementsById(ageResults);
+    
+
+
+}
+
+ function resetAge(){
+    document.getElementById('ageInDays').remove();
+}
